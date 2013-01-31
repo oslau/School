97146094dbb83cda321b98bb13f341ff898370c4
Olivia Lau <oliviaslau@gmail.com>
2013-01-31 15:49:59 -0800

    Working for individual time steps.

--- Testing.R
+++ Testing.R
@@ -17,8 +17,12 @@ totcells = grid.r * grid.c	##total number of cells
 ##############################
 ###GENERATE RANDOM CAR GRID###
 ##############################
-colors = c(rep(0, totcells-ncars), rep(1, ncars/2), rep(2, ncars/2))
-grid = matrix(sample(colors, totcells), nrow = grid.r)
+
+genGrid = function(grid.r, grid.c, rho = .5){
+	colors = c(rep(0, totcells-ncars), rep(1, ncars/2), rep(2, ncars/2))
+	grid = matrix(sample(colors, totcells), nrow = grid.r)
+}
+
 image(t(grid[grid.r:1,]), axes = FALSE, col = c("white", "blue", "red"))
 #grid.coords = melt(grid)
 #names(grid.coords) <- c("i", "j", "col")
@@ -26,56 +30,12 @@ image(t(grid[grid.r:1,]), axes = FALSE, col = c("white", "blue", "red"))
 ###################
 ###MOVE THAT CAR###
 ###################
-General idea:
-#move = function(grid, time) 
-#if t= odd
-##look at blue cars
-### move cars by checking if space is available
-#if t = even
-##look at red cars
-##move cars by checking if space is available
-
-##This function changes the coordinates according to specified direction
-next.pos = function(ij, direction){
-	pos = switch(direction,
-		right = cbind(ij[,1], ij[,2]+1),
-		up = cbind(ij[,1]-1, ij[,2])
-	)
-	pos = off.grid(pos)
-	return(pos)}
-
-##This function checks if the move generated aboce is off the grid
-##If so, it will wrap around.
-off.grid = function(ij){
-	ij[ij[ , 1] > grid.r, 1] = 1
-	ij[ij[ , 1] == 0, 1] = grid.r
-	ij[ij[ , 2] > grid.c, 2] = 1
-	ij[ij[ , 2] == 0, 2] = grid.c
-	return(ij)
-}
-
-#GENERAL FUNCTION
-#swap = function(empty.spot, car.spot){
-#	grid[empty.spot[1], empty.spot[2]] = grid[car.spot[1], car.spot[2]]
-#	grid[car.spot[1], car.spot[2]] = 0
-#	return(grid)
-#}
-
-move = function(grid, time){
-	if(time %% 2 == 0){		##red cars
-		direction = "right"
-		car.pos = which(grid == 2, arr.ind = TRUE)
-	}
-	else{		##blue cars
-		direction = "up"
-		car.pos = which(grid == 1, arr.ind = TRUE)
-	}
-	potential = next.pos(car.pos, direction)
-	##check next pos
-	new.grid = swap(car.pos, potential)
-	velocity = totcells - sum(grid == new.grid)
-}
-
+##This function changes the coordinates next potential position
+next.pos = function(ij, n.row){
+	up = cbind(ij[,1]-1, ij[,2])
+	up[up[ , 1] == 0, 1] = n.row
+	return(up)}
+	
 swap = function(car.pos, potential, grid){
 	values = apply(potential, 1, function(x) grid[x[1], x[2]])
 	t = which(values == 0)
@@ -85,4 +45,32 @@ swap = function(car.pos, potential, grid){
 		grid[swap[i,3], swap[i,4]] = 0
 	}
 	return(grid)
+}
+
+move = function(grid, time){
+	d = dim(grid)
+	if(time %% 2 == 0){		##red cars
+		grid = t(grid[ , ncol(grid):1])
+		car.pos = which(grid == 2, arr.ind = TRUE)
+		potential = next.pos(car.pos, nrow(grid))
+		new.grid = swap(car.pos, potential, grid)
+		new.grid = t(new.grid[nrow(new.grid):1,])
+	}
+	else{		##blue cars
+		car.pos = which(grid == 1, arr.ind = TRUE)
+		potential = next.pos(car.pos, nrow(grid))
+		new.grid = swap(car.pos, potential, grid)
+	}
+	return(new.grid)
+}
+
+simTraffic = function(grid, t = 100){
+	velocity = integer(t)
+	for(i in 1:t){
+		new.grid = move(grid, i)
+		velocity[i] = length(grid) - sum(new.grid == grid)
+		grid = new.grid
+	}
+	plot(grid)
+	return(velocity)
 }
